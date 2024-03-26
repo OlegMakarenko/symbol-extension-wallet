b6b178c28c5987d3a0cc27c7a0930c32482ed359
Oleg Makarenko <olegomm@gmail.com>
2024-03-26 20:07:24 +0100

    [wallet/extension] task: add symbol constants

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,7 @@
+export const DEFAULT_ACCOUNT_NAME = 'My Account';
+
+export const MAX_SEED_ACCOUNTS_PER_NETWORK = 10;
+
 export const Events = {
     CONFIRMED_TRANSACTION: 'event.listener.confirmed',
     PARTIAL_TRANSACTION: 'event.listener.partial',
@@ -5,24 +9,10 @@ export const Events = {
     LOGIN: 'event.app.login',
 };
 
-export const NetworkType = {
-    MAIN_NET: 104,
-    TEST_NET: 152,
-};
-
-export const NetworkIdentifier = {
-    MAIN_NET: 'mainnet',
-    TEST_NET: 'testnet',
-};
-
 export const WalletAccountType = {
     SEED: 'seed',
     EXTERNAL: 'external'
 }
-
-export const DEFAULT_ACCOUNT_NAME = 'My Account';
-
-export const MAX_SEED_ACCOUNTS_PER_NETWORK = 10;
 
 export const TransactionType = {
     RESERVED: 0,
@@ -52,6 +42,159 @@ export const TransactionType = {
     VOTING_KEY_LINK: 16707,
     NODE_KEY_LINK: 16972,
     PERSISTENT_DELEGATION_REQUEST: 'PERSISTENT_DELEGATION_REQUEST',
+};
+
+export const NetworkType = {
+    MAIN_NET: 104,
+    TEST_NET: 152,
+};
+
+export const NetworkIdentifier = {
+    MAIN_NET: 'mainnet',
+    TEST_NET: 'testnet',
+};
+
+export const Message = {
+    UNLIMITED: '∞',
+    UNAVAILABLE: 'v_na',
+    INFINITY: '∞',
+    MOSAIC: 'v_moasic',
+    ADDRESS: 'v_address',
+    NO_ALIAS: 'v_noAlias',
+    ACTIVE: 'v_active',
+    INACTIVE: 'v_inactive',
+    UNKNOWN: 'v_unknown',
+    EXPIRED: 'v_expired',
+    UNCONFIRMED: 'unconfirmed',
+    CONFIRMED: 'confirmed',
+};
+
+export const MosaicSupplyChangeAction = {
+    Decrease: 0,
+    Increase: 1
+}
+
+export const MosaicSupplyChangeActionMessage = {
+    [MosaicSupplyChangeAction.Increase]: 'v_increase',
+    [MosaicSupplyChangeAction.Decrease]: 'v_decrease',
+};
+
+export const NamespaceRegistrationType = {
+    RootNamespace: 0,
+    SubNamespace: 1
+}
+
+export const NamespaceRegistrationTypeMessage = {
+    [NamespaceRegistrationType.RootNamespace]: 'v_rootNamespace',
+    [NamespaceRegistrationType.SubNamespace]: 'v_subNamespace',
+};
+
+export const AliasAction = {
+    Link: 1,
+    Unlink: 0
+}
+
+export const AliasActionMessage = {
+    [AliasAction.Link]: 'v_link',
+    [AliasAction.Unlink]: 'v_unlink',
+};
+
+export const LinkAction = {
+    Link: 1,
+    Unlink: 0
+}
+
+export const LinkActionMessage = {
+    [LinkAction.Link]: 'v_link',
+    [LinkAction.Unlink]: 'v_unlink',
+};
+
+export const LockHashAlgorithm = {
+    Op_Sha3_256: 0,
+    Op_Hash_160: 1,
+    Op_Hash_256: 2
+}
+
+export const LockHashAlgorithmMessage = {
+    [LockHashAlgorithm.Op_Sha3_256]: 'Sha3 256',
+    [LockHashAlgorithm.Op_Hash_160]: 'Hash 160',
+    [LockHashAlgorithm.Op_Hash_256]: 'Hash 256',
+};
+
+export const MetadataType = {
+    Account: 0,
+    Mosaic: 1,
+    Namespace: 2
+}
+
+export const MetadataTypeMessage = {
+    [MetadataType.Account]: 'Account',
+    [MetadataType.Mosaic]: 'Mosaic',
+    [MetadataType.Namespace]: 'Namespace',
+};
+
+export const AddressRestrictionFlag = {
+    AllowIncomingAddress: 1,
+    AllowOutgoingAddress: 16385,
+    BlockIncomingAddress: 32769,
+    BlockOutgoingAddress: 49153
+}
+
+export const AddressRestrictionFlagMessage = {
+    [AddressRestrictionFlag.AllowIncomingAddress]: 'v_AllowIncomingAddresses',
+    [AddressRestrictionFlag.AllowOutgoingAddress]: 'v_AllowOutgoingAddresses',
+    [AddressRestrictionFlag.BlockIncomingAddress]: 'v_BlockIncomingAddresses',
+    [AddressRestrictionFlag.BlockOutgoingAddress]: 'v_BlockOutgoingAddresses',
+};
+
+export const MosaicRestrictionFlag = {
+    AllowMosaic: 2,
+    BlockMosaic: 32770
+}
+
+export const MosaicRestrictionFlagMessage = {
+    [MosaicRestrictionFlag.AllowMosaic]: 'v_AllowMosaics',
+    [MosaicRestrictionFlag.BlockMosaic]: 'v_BlockMosaics',
+};
+
+export const OperationRestrictionFlag = {
+    AllowOutgoingTransactionType: 16388,
+    BlockOutgoingTransactionType: 49156
+}
+
+export const OperationRestrictionFlagMessage = {
+    [OperationRestrictionFlag.AllowOutgoingTransactionType]: 'v_AllowOutgoingTransactions',
+    [OperationRestrictionFlag.BlockOutgoingTransactionType]: 'v_BlockOutgoingTransactions',
+};
+
+export const MosaicRestrictionEntryType = {
+    ADDRESS: 0,
+    GLOBAL: 1
+}
+
+export const MosaicRestrictionEntryTypeMessage = {
+    [MosaicRestrictionEntryType.ADDRESS]: 'v_MosaicAddressRestriction',
+    [MosaicRestrictionEntryType.GLOBAL]: 'v_MosaicGlobalRestriction',
+};
+
+export const MosaicRestrictionType = {
+    NONE: 0,
+    EQ: 1,
+    NE: 2,
+    LT: 3,
+    LE: 4,
+    GT: 5,
+    GE: 6
+}
+
+export const MosaicRestrictionTypeMessage = {
+    [MosaicRestrictionType.EQ]: 'v_mosaicRestrictionTypeEQ',
+    [MosaicRestrictionType.GE]: 'v_mosaicRestrictionTypeGE',
+    [MosaicRestrictionType.GT]: 'v_mosaicRestrictionTypeGT',
+    [MosaicRestrictionType.LE]: 'v_mosaicRestrictionTypeLE',
+    [MosaicRestrictionType.LT]: 'v_mosaicRestrictionTypeLT',
+    [MosaicRestrictionType.NE]: 'v_mosaicRestrictionTypeNE',
+    [MosaicRestrictionType.NONE]: 'v_mosaicRestrictionTypeNONE',
 };
 
 export const MosaicFlags = {
