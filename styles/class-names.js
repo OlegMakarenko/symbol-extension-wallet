f57cc8e9204a54a0e042c1eff786a6faf89e7339
Oleg Makarenko <olegomm@gmail.com>
2024-03-29 21:40:07 +0100

    [wallet/extension] task: add global class names to style control components

---
+++ styles/class-names.js
@@ -0,0 +1,10 @@
+export const classNames = {
+    controlWrapper: [
+        'border-secondary',
+        'data-[hover=true]:border-secondary-600',
+        'group-data-[invalid=true]:border-danger'
+    ],
+    controlLabel: [''],
+    button: ['font-button', 'uppercase'],
+    popup: ['bg-black']
+}
