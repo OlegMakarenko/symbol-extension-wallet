46472c089e490ab9cfac9f2f606a3762be3cbd9a
Oleg Makarenko <olegomm@gmail.com>
2024-05-22 17:50:55 +0200

    [wallet/extension] feat: restyle ActionRequest screen, add website info

--- screens/ActionRequest.jsx
+++ screens/ActionRequest.jsx
@@ -1,22 +1,12 @@
 import _ from 'lodash';
-import store, { connect } from '@/store';
-import { useDataManager, useInit, usePasscode } from '@/utils/hooks';
-import { handleError } from '@/utils/helper';
+import { usePasscode } from '@/utils/hooks';
 import { useLocation } from 'react-router-dom';
 import { Button, Screen, FormItem, TitleBar, useRouter, Card } from '@/components/index';
 import { $t } from '@/localization';
 import { WalletController } from '@/core/WalletController';
 import { ExtensionRpcMethods } from '@/constants';
-import { Divider } from '@nextui-org/react';
 
-export const ActionRequest = connect((state) => ({
-    currentAccount: state.account.current,
-    cosignatories: state.account.cosignatories,
-    isAccountReady: state.account.isReady,
-    isWalletReady: state.wallet.isReady,
-
-}))(function ActionRequest(props) {
-    const { currentAccount, isAccountReady, isWalletReady } = props;
+export const ActionRequest = () => {
     const router = useRouter();
     const { state } = useLocation();
     const isPermissionRequest = state.method === ExtensionRpcMethods.requestPermission;
@@ -33,26 +23,14 @@ export const ActionRequest = connect((state) => ({
     }
     const [Passcode, confirmAction] = usePasscode(handleAction);
 
-    const [loadState, isStateLoading] = useDataManager(
-        async () => {
-            await store.dispatchAction({ type: 'wallet/fetchAll' });
-        },
-        null,
-        handleError
-    );
-    useInit(loadState, isWalletReady, [currentAccount]);
-
     const clearAndLeave = async () => {
         await WalletController.removeRequests([state.id]);
         router.goToHome();
     }
 
-    const isLoading = !isAccountReady || isStateLoading;
-
     return (
         <Screen
             titleBar={<TitleBar hasAccountSelector hasSettingsButton />}
-            isLoading={isLoading}
             bottomComponent={
                 <>
                     <FormItem>
@@ -65,23 +43,25 @@ export const ActionRequest = connect((state) => ({
             }
         >
             <FormItem>
-                <h2>{$t('s_actionRequest_title')}</h2>
-                <p>{$t('s_actionRequest_description')}</p>
+                <h2>{title}</h2>
+                <p>{description}</p>
             </FormItem>
-            <FormItem>
+            <div className="flex-1 flex flex-col items-center justify-around">
+                <div className="flex flex-col items-center text-center mb-4">
+                    <img src={state.sender.icon} className="bg-white h-32 mx-auto mb-4 rounded-full" />
+                    <p className="font-mono leading-tight w-3/4 truncate ...">{state.sender.title}</p>
+                    <p className="font-mono opacity-70 leading-tight w-3/4 truncate ...">{state.sender.origin}</p>
+                </div>
                 <Card>
-                    <h3>{title}</h3>
-                    <p>{description}</p>
                     {isPermissionRequest && (
                         <div>
-                            <Divider className="my-4" />
                             <h3>{$t(`extensionPermission_${state.payload}`)}</h3>
                             <p>{$t(`extensionPermission_${state.payload}_description`)}</p>
                         </div>
                     )}
                 </Card>
-            </FormItem>
+            </div>
             <Passcode />
         </Screen>
     );
-});
+};
